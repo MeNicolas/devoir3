2b9a46dc321ddbf67e36d92ba3eabd83232404ad
Nicolas Dalbard <dalbard.nicolas@me.com>
2020-11-30 14:18:45 +0000

    a

--- manager/config.py
+++ manager/config.py
@@ -10,4 +10,5 @@ testing = os.getenv("TESTING")
 
 package_dir = os.path.dirname(__file__)
 
-path_data = os.path.join(package_dir, os.getenv("CSV_FILENAME"))
+path_data = os.path.join(
+	package_dir, os.getenv("CSV_FILENAME"))
